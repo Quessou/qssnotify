69fe66f1fadcbd386aaffc91c7923bc7dd968978
Maxime <maximemikotajewski@gmail.com>
2023-06-20 22:40:48 +0200

    removed useless carriage return

--- src/actions/add.rs
+++ src/actions/add.rs
@@ -9,7 +9,10 @@ pub async fn add_sentence(settings: &Settings) -> Result<(), std::io::Error> {
     let temp_file = utils::create_tmp_file();
 
     open_file_in_editor(&settings.editor, temp_file.path().to_str().unwrap())?;
-    let content = read_file_content(temp_file.path().to_str().unwrap()).await?;
+    let content = read_file_content(temp_file.path().to_str().unwrap())
+        .await?
+        .trim()
+        .to_owned();
     tracing::info!("Sentence to add : {}", content);
 
     utils::append_sentence_to_data_file(content.into()).await
